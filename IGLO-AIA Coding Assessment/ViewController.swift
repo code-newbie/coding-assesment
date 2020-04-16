e89d3bd73208393a79dc4ef98a102610a246dbdd
Vebby Clarissa <vebbyclarissa@gmail.com>
2020-04-16 20:35:03 +0700

    Initial Commit

---
+++ IGLO-AIA Coding Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGLO-AIA Coding Assessment
+//
+//  Created by Vebby Clarissa on 16/04/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
