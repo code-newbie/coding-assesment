5df0075815e2a56bd48150fd9eefee24fe7833c0
ipardosi16 <imanuelpardosi@gmail.com>
2020-04-29 21:47:34 +0700

    Initial project

---
+++ aia-ios-assesment/aia-ios-assesment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aia-ios-assesment
+//
+//  Created by Imanuel Pardosi on 29/04/20.
+//  Copyright © 2020 Imanuel Pardosi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
